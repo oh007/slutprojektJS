5b145c3e5b45fc6596d70ae443e10cde2c96eabe
oh007 <81170807+oh007@users.noreply.github.com>
2022-10-31 20:14:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+'use strict'
+// monster array med object ett som innehåller monster
+const monsterArray = {
+    monster: [
+        {
+            name: Tentacel,
+            color: Yellow,
+            type: mid
+        },
+        {
+            name: Basilisk,
+            color: Green,
+            type: Large, 
+        },
+        {
+            name: Beelzebub,
+            color: Black,
+            type: Small,
+        },
+        {
+            name: Automatons,
+            color: Pink,
+            type: Large,
+        },
+        
+        editMonsterapperance: function() {
+        }
+    ],
+    // funktion för att lägga till monster
+    addMonster: function(monster) {
+
+    },
+
+
+}
+
+// redigera monster och kunna lägga till antal tentakler, antal ögon, antal horn och antal armar
+let 
+// hämtar alla monster och sorterar dom på färger
+ function monsterByColor(monsterArray.monster.color) {
+
+}
+// hämtar alla monster och sorterar dom efter typ
+typeofMonster: function(monsterArray.monster.type) {
+    
+} 
+editmonster: function() {
+     
+}
+
+
+
+
+
+
+
+    
